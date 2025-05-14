c3067c29c185582e4aaa94c1adebb32afa42306a
Zainaaz Hansa <zainaazhansa@gmail.com>
2025-05-14 22:50:03 +0200

    Routes, middlewares & controllers

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,24 @@
-ECHO is on.
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json()); // Parses JSON bodies
+
+// Import routes
+const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
+const cardRoutes = require('./routes/cardRoutes');
+
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/cards', cardRoutes);
+
+// Server start
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Flexitec backend running on port ${PORT}`);
+});
